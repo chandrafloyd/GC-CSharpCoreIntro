8cc46bba4746a69259bd56e26ab97fda8fffb109
chandrafloyd <chandra_floyd@yahoo.com>
2018-01-23 14:02:43 -0500

    first commit

---
+++ CSharpCoreIntro/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharpCoreIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            double Input;
+
+            // input 
+            Console.WriteLine("Please enter a number");
+            Input = float.Parse(Console.ReadLine());
+
+            // processing
+            //Input = Input + 1; //canalso write Inpt+=1; Input++; to get the same effect
+            Input = Math.Sqrt(Input); //math returns double
+
+            //output
+            Console.WriteLine($"Output is equal to: {Input}");
+            Console.Read();
+        }
+    }
+}
